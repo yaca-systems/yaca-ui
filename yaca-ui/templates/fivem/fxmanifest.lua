4f81845c33d3695722d3e7b32616f8f1e1446415
Blue <florian.uaz@web.de>
2024-11-30 14:53:32 +0100

    feat: add fivem wrapper

---
+++ yaca-ui/templates/fivem/fxmanifest.lua
@@ -0,0 +1,12 @@
+games { 'gta5', 'rdr3' }
+fx_version 'cerulean'
+
+ui_page 'index.html'
+
+files {
+    'index.html',
+    'config.json',
+    'assets/*'
+}
+
+client_script 'client.js'
--- yaca-ui/vite.config.ts
+++ yaca-ui/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [vue()],
     base: './',
     build: {
-        outDir: './dist/assets',
+        outDir: './dist',
         emptyOutDir: true
     },
     resolve: {
