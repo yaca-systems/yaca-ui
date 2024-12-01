8dddc587b732d97eff10b3c248cd2c50fb520e28
Blue <florian.uaz@web.de>
2024-12-01 18:54:33 +0100

    feat: add no plugin active banner

--- yaca-ui/src/main.ts
+++ yaca-ui/src/main.ts
@@ -11,6 +11,10 @@ import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
 import "./assets/style.css";
+import {createI18n} from "vue-i18n";
+import {YaCAConfig} from "./config";
+import {locale_en} from "./locale/en";
+import {locale_de} from "./locale/de";
 
 // Add the routes for each page
 const routes = [
@@ -24,7 +28,6 @@ const router = createRouter({
     history: createWebHashHistory(),
     routes
 });
-
 
 const vuetify = createVuetify({
     theme: {
@@ -42,6 +45,15 @@ const vuetify = createVuetify({
     components: {
         ...components
     }
+});
+
+const i18n = createI18n({
+    locale: YaCAConfig.LANGUAGE,
+    fallbackLocale: 'en',
+    messages: {
+        en: locale_en,
+        de: locale_de
+    }
 })
 
-createApp(App).use(vuetify).use(router).mount('#app');
+createApp(App).use(vuetify).use(router).use(i18n).mount('#app');
