5d4a3370859fe2972941753d32146d222b0ffb60
Blue <florian.uaz@web.de>
2024-12-01 18:54:33 +0100

    feat: add no plugin active banner

--- yaca-ui/src/App.vue
+++ yaca-ui/src/App.vue
@@ -1,5 +1,8 @@
 <template>
     <v-app>
+        <NoPluginFullScreen v-if="!isPluginActive && YaCAConfig.NO_PLUGIN_ACTIVE_FULLSCREEN" />
+        <NoPluginBanner v-else-if="!isPluginActive" />
+
         <div class="w-full h-full bg-transparent">
             <router-view></router-view>
         </div>
@@ -7,7 +10,19 @@
 </template>
 
 <script lang="ts" setup>
+import { onMounted, ref } from "vue";
+import NoPluginFullScreen from "@/components/noplugin/NoPluginFullScreen.vue";
+import { YaCAConfig } from "@/config";
+import { GameService } from "@/services/game-service";
+import NoPluginBanner from "@/components/noplugin/NoPluginBanner.vue";
 
+const isPluginActive = ref(false);
+
+onMounted(() => {
+    GameService.on("webview:yaca:isActive", (state: boolean) => {
+        isPluginActive.value = state;
+    });
+});
 </script>
 <style>
 /* Start Fix for fivem to have transparent backgrpund */
---
+++ yaca-ui/src/components/noplugin/NoPluginBanner.vue
@@ -0,0 +1,9 @@
+﻿<template>
+  <div class="w-100 d-flex align-center justify-center" style="background: #46ce47; height: 4vh">
+    <span class="text-black font-weight-bold">{{ $t('NO_PLUGIN_MESSAGE_HEADER') }} | {{ $t('NO_PLUGIN_MESSAGE') }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
