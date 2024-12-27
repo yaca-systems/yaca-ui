e0d838984516bbdddc0727647eced1fc4b8997b9
fabiouh <fabian.r.stifter@gmail.com>
2024-12-27 20:16:45 +0100

    white & margin

--- yaca-ui/src/components/noplugin/NoPluginFullScreen.vue
+++ yaca-ui/src/components/noplugin/NoPluginFullScreen.vue
@@ -1,7 +1,7 @@
 ﻿<template>
   <div class="w-100 h-100 overflow-hidden d-flex justify-center align-center">
-    <div class="w-100 h-100 d-flex justify-center align-center flex-column" style="background: rgba(0, 0, 0, 0.6); border: 4px solid #46ce47cc; box-shadow: 0px 0px 24px 0px rgba(70,206,71,0.5);">
-      <img src="../../assets/logo.svg" width="150" alt="logo">
+    <div class="w-100 h-100 d-flex justify-center align-center flex-column text-white" style="background: rgba(0, 0, 0, 0.6); border: 4px solid #46ce47cc; box-shadow: 0px 0px 24px 0px rgba(70,206,71,0.5);">
+      <img src="../../assets/logo.svg" width="150" alt="logo" class="mb-4">
       <h3>{{ LocaleService.GetLocale("no_plugin_active_header") }}</h3>
       <p>{{ LocaleService.GetLocale("no_plugin_active_body") }}</p>
     </div>
