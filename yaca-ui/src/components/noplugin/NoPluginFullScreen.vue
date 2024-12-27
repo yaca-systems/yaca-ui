875317a827f6a788fe96013776276ff4a0da7e43
fabiouh <fabian.r.stifter@gmail.com>
2024-12-27 20:18:09 +0100

    darker bg

--- yaca-ui/src/components/noplugin/NoPluginFullScreen.vue
+++ yaca-ui/src/components/noplugin/NoPluginFullScreen.vue
@@ -1,6 +1,6 @@
 ﻿<template>
   <div class="w-100 h-100 overflow-hidden d-flex justify-center align-center">
-    <div class="w-100 h-100 d-flex justify-center align-center flex-column text-white" style="background: rgba(0, 0, 0, 0.6); border: 4px solid #46ce47cc; box-shadow: 0px 0px 24px 0px rgba(70,206,71,0.5);">
+    <div class="w-100 h-100 d-flex justify-center align-center flex-column text-white" style="background: rgba(0, 0, 0, 0.8); border: 4px solid #46ce47cc; box-shadow: 0px 0px 24px 0px rgba(70,206,71,0.5);">
       <img src="../../assets/logo.svg" width="150" alt="logo" class="mb-4">
       <h3>{{ LocaleService.GetLocale("no_plugin_active_header") }}</h3>
       <p>{{ LocaleService.GetLocale("no_plugin_active_body") }}</p>
